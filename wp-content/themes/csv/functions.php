e4b29903065d5233c46eeb820a766717d266f2b7
Melissa Galvan <mel.gvn69@gmail.com>
2022-06-21 16:34:13 +0200

    load script and style files

--- wp-content/themes/csv/functions.php
+++ wp-content/themes/csv/functions.php
@@ -1,4 +1,11 @@
 <?php
+// Enqueue script and style
+add_action( 'wp_enqueue_scripts', 'load_styles');
+function load_styles() {
+    wp_enqueue_style( 'style', get_template_directory_uri().'/assets/css/style.css' );
+    wp_enqueue_script( 'script', get_template_directory_uri().'/assets/js/functions.js' );
+}
+
 function register_my_menu()
 {
   register_nav_menu('main-menu', 'Menu principal');
